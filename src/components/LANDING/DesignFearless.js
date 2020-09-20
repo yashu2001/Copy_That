bd5f0b94ac2be9ea1052b638b081876a34889f61
Yash.S.Narang <y_narang@yahoo.in>
2020-09-21 02:44:48 +0530

    Page one work started

---
+++ src/components/LANDING/DesignFearless.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+
+export default function Second() {
+  return (
+    <>
+      <img
+        src="/pattern.png"
+        style={{ width: "98vw", objectFit: "cover" }}
+        alt="this is not available"
+      />
+      <Container>
+        <h1 class="margin-top-120 text-center h1-after">Design Fearlessly</h1>
+        
+      </Container>
+    </>
+  );
+}
--- src/components/LANDING/index.js
+++ src/components/LANDING/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../COMMON/Navbar";
+import Footer from "../COMMON/Footer";
+import Second from "./DesignFearless";
 export default function Index() {
-    return (
-        <div>
-            Landing
-        </div>
-    )
+  return (
+    <>
+      <Navbar activatedLink="home" />
+      <Second />
+      <Footer />
+    </>
+  );
 }

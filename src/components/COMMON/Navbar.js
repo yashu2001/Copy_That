adc64970c509a4e59402e3c557051b6ea29aa9f5
Yash.S.Narang <y_narang@yahoo.in>
2020-09-21 02:58:46 +0530

    Fixed navigation issue in navbar

--- src/components/COMMON/Navbar.js
+++ src/components/COMMON/Navbar.js
@@ -31,32 +31,35 @@ export default function NAVBAR({ activatedLink }) {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             <Nav className="nav-grp">
-              <Nav.Link
-                href="/"
-                className={activatedLink === "home" ? "active" : ""}
-              >
-                <span className={activatedLink === "home" ? "active-span" : ""}>
-                  HOME
-                </span>
-              </Nav.Link>
-              <Nav.Link
-                href="faq"
-                className={activatedLink === "faq" ? "active" : ""}
-              >
-                <span className={activatedLink === "faq" ? "active-span" : ""}>
-                  FAQS
-                </span>
-              </Nav.Link>
-              <Nav.Link
-                href="/contact"
-                className={activatedLink === "contact" ? "active" : ""}
-              >
-                <span
-                  className={activatedLink === "contact" ? "active-span" : ""}
+              <Link to="/">
+                <Nav.Link className={activatedLink === "home" ? "active" : ""}>
+                  <span
+                    className={activatedLink === "home" ? "active-span" : ""}
+                  >
+                    HOME
+                  </span>
+                </Nav.Link>
+              </Link>
+              <Link to="/faq">
+                <Nav.Link className={activatedLink === "faq" ? "active" : ""}>
+                  <span
+                    className={activatedLink === "faq" ? "active-span" : ""}
+                  >
+                    FAQS
+                  </span>
+                </Nav.Link>
+              </Link>
+              <Link to="/contact">
+                <Nav.Link
+                  className={activatedLink === "contact" ? "active" : ""}
                 >
-                  CONTACT
-                </span>
-              </Nav.Link>
+                  <span
+                    className={activatedLink === "contact" ? "active-span" : ""}
+                  >
+                    CONTACT
+                  </span>
+                </Nav.Link>
+              </Link>
             </Nav>
             <Nav>
               <Link to="/">
